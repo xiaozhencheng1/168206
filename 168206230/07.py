36e0f6f594d4246b91463b861c9e21adbe9be7d1
xiaozhencheng1 <44491886+xiaozhencheng1@users.noreply.github.com>
2018-12-29 16:42:47 +0800

    Delete 07.py

--- 168206230/07.py
+++
@@ -1,4 +0,0 @@
-list = ["A", "B", "C", "D"]
-for i in range(len(list)):    
-    if (list[i] !="A") + (list[i] =="D") + (list[i] =="B") + (list[i] !="C") == 1:	           
-        print("小偷是"  + list[i])
