3c13a1f718a1bf2a9c31916b4b67b298bec21f8e
xiaozhencheng1 <44491886+xiaozhencheng1@users.noreply.github.com>
2018-12-29 16:38:31 +0800

    Update 07.py

--- 168206230/07.py
+++ 168206230/07.py
@@ -1,4 +1,4 @@
 list = ["A", "B", "C", "D"]
 for i in range(len(list)):    
-    if (list[i] !="A") + (list[i] =="D") + (list[i] =="B") + (list[i] !="D") == 1:	           
+    if (list[i] !="A") + (list[i] =="D") + (list[i] =="B") + (list[i] !="C") == 1:	           
         print("小偷是"  + list[i])
